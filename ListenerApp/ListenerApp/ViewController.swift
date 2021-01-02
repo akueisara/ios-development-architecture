dff7d6bce2db58e836bc6a1a8f05e8c0be821061
Kuei-Jung Hu <akueisara39@gmail.com>
2021-01-03 04:20:25 +0900

    7.6 Display the message, part 2

--- ListenerApp/ListenerApp/ViewController.swift
+++ ListenerApp/ListenerApp/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var label: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "MessageReceived"), object: nil, queue: OperationQueue.main) { notification in
+			if let message = notification.object as? String {
+				self.label.text = message.removingPercentEncoding
+			}
+		}
 	}
 
 
