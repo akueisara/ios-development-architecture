b7aef933ca3215f409194bd3256f206ee3e741fc
Kuei-Jung Hu <akueisara39@gmail.com>
2021-01-03 00:36:01 +0900

    3.2 The model-view-controller

---
+++ SingleViewApp/SingleViewApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Kuei-Jung Hu on 2021/01/03.
+//
+
+import UIKit
+
+/// MVC Design Patten
+/// 
+/// The Model:
+/// Data
+/// Logic to manipulate data
+///
+/// The View:
+/// Presents data
+/// Sends user events to the Controller
+///
+/// The Controller:
+/// Mediates between the View and the Model
+/// Prepares data for visual presentation
+/// Converts data before passing it back to the Model
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
